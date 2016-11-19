6695156b84695671a62f28fa7d648b7a4dbe3bf2
Meir Snyder <msny36@gmail.com>
2016-11-19 16:59:42 -0500

    trying results + '[geometry]'

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def makeYqlQuery(req):
 
 
 def makeWebhookResult(data):
-    query = data.get(results['geometry'])
+    query = data.get('results'+'[geometry]')
     print("query")
     print(query)
     
